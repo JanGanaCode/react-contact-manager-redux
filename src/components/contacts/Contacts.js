fb320cf6fc05edf59eed5b1aaba218a01f3bc4b7
Jan Gana <jan.gana@hiring-hub.com>
2018-11-16 02:34:20 +0000

    Update Contact

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import { getContacts } from '../../actions/contactActions';
 
 class Contacts extends Component {
@@ -23,11 +22,6 @@ class Contacts extends Component {
     );
   }
 }
-
-Contact.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  getContacts: PropTypes.func.isRequired
-};
 
 const mapStateToProps = (state) => ({
   contacts: state.contact.contacts
